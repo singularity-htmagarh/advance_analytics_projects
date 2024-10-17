dec9c54ed0ff8788a3b518a3f904c62c33415b41
htmagarh <htmagar.university@gmail.com>
2024-10-17 13:07:12 -0400

    VS Code Updates - On 17th Oct 2024

--- decision_tree_random_forest.R
+++ decision_tree_random_forest.R
@@ -12,16 +12,13 @@ head(College)
 ## EDA
 ### Some basic data exploration before we being building our models
 ### ** Scatterplot of graduation rate vs room and boarding costs
-
 ggplot(df, aes(Room.Board, Grad.Rate)) + geom_point(aes(color=Private))
 
 ## It makes sense to see that Private Colleges have higher Room and Boarding Costs
 ## ** Histogram of full time undergrad students, colored by Private. **
-
-ggplot(df, aes(F.Undergrad))+geom_histogram(aes(fill=Private),color="black",bins=50)
+ggplot(df, aes(F.Undergrad)) + geom_histogram(aes(fill=Private),color="black",bins=50)
 
 ## ** histogram of graduation rate, colored by Private **
-
 ggplot(df, aes(Grad.Rate)) + geom_histogram(aes(fill=Private),color="black", bin=50)
 
 ## There is a college with a graduation rate above 100%. Lets find that out and fix it
@@ -29,35 +26,27 @@ ggplot(df, aes(Grad.Rate)) + geom_histogram(aes(fill=Private),color="black", bin
 subset(df, Grad.Rate>100)
 
 ## Fix:
-
 df['Cazenovia College','Grad.Rate'] <- 100
 
 ## Decision Tree
-
 tree = rpart(Private ~ .,method="class",data=df)
 tree.preds = predict(tree,df)
 head(tree.preds)
 
 ## Creating a column "Private", with the variables Yes/No to indicate if the college is private or not, to match our original dataframe so that we can compare the results easily
-
 tree.preds = as.data.frame(tree.preds)
 tree.preds$Private = ifelse(tree.preds$Yes > 0.5,"Yes","No")
 
 ## Checking out our confusion Matrix
-
 table(tree.preds$Private, df$Private)
 
 library(rpart.plot)
 prp(tree)
 
 ## RANDOM FOREST
-
 library(randomForest)
 
 rf.model = randomForest(Private ~ .,data=df, importance = TRUE)
 
 p <- predict(rf.model, test)
 table(p,test$Private)
-
-
-
